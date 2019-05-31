48bbd766fc0893b197063186d19cfbc69a6adc6e
dromanovsky <d.romanovskybox@gmail.com>
2019-05-31 10:15:07 +0300

    Printer class

--- printer.rb
+++ printer.rb
@@ -1,5 +1,34 @@
 # frozen_string_literal: true
 
 class Printer
-  def self.run(data); end
+  attr_accessor :data_storages
+
+  def initialize(data_storages)
+    @data_storages = data_storages
+  end
+
+  def self.run(data_storages)
+    new(data_storages).run
+  end
+
+  def run
+    data_storages.each do |data_storage|
+      render_data_storage(data_storage)
+    end
+  end
+
+  private
+
+  def render_data_storage(data_storage)
+    data_storage.printable_data.each do |data_batch|
+      puts data_batch[:caption]
+      render_data_rows(data_batch)
+    end
+  end
+
+  def render_data_rows(data_batch)
+    data_batch[:data].each do |data_row|
+      puts "#{data_row.first} - #{data_row.last} visits"
+    end
+  end
 end
